f3d8f29451b9ecff32a8b909e497480a7340c0d7
Tanishq Bhardwaj <tanishqbhardwaj.tb@gmail.com>
2026-03-20 08:59:10 +0530

    add qdrant cloud connection

---
+++ 02_vector/01_cloud/cloud_cluster.py
@@ -0,0 +1,12 @@
+import os
+from qdrant_client import QdrantClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = QdrantClient(
+    url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY"),
+)
+
+print(client.get_collections())
